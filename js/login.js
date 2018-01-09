9ed034945a46234aa53d842f0b386be87b32353d
vaneshita <144995@unsaac.edu.pe>
2018-01-09 11:14:59 -0500

    vista de red social y arreglando css en login y sign up

--- js/login.js
+++ js/login.js
@@ -1,8 +1,13 @@
-$('.toggle').click(function() {
-  $('.formulario').animate({
-    height: 'toggle',
-    'padding-top': 'toggle',
-    'padding-bottom': 'toggle',
-    opacity: 'toggle'
-  }, 'slow');
+$(document).ready(function() {
+  $('.toggle').click(function() {
+    $('.formulario').animate({
+      height: 'toggle',
+      'padding-top': 'toggle',
+      'padding-bottom': 'toggle',
+      opacity: 'toggle'
+    }, 'slow');
+  });
+  $('input[type="submit"]').click(function() {
+    window.location.href = '../views/redSocialSmile.html';
+  });
 });
